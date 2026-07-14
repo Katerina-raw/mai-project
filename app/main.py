39974d8b10606ae378a817a4b366f70c097621f7
yymato <070913a@gmail.com>
2026-07-14 12:47:40 +0300

    Prompt rewritten, AI class created. The AI receives data block by block and processes them into JSON.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from app.ai import request_to_qwen
+from app.ai import AI
 from app.utiles import decode_base64_data, encode_base64_data
 from app.utiles import extract_zip_data
 
@@ -9,9 +9,6 @@ app = FastAPI()
 
 class Subject(BaseModel):
     ID: int
-    Lecture_hours: int
-    Practice_hours: int
-    Lab_hours: int
     Subject_name: str
     Subject_description: str
     Blocks: list[dict]
@@ -21,8 +18,8 @@ class Subject(BaseModel):
 def register(subject: Subject):
     bytes_archives = decode_base64_data(subject.Archives)
     data = extract_zip_data(bytes_archives)
-    response = request_to_qwen(data, subject.Lecture_hours)
-
+    ai = AI()
     return {"status": "success",
-            "message": "Successfully done"}
+            "message": "Successfully done",
+            "answer": ai.orchestrate_course_generation(subject.Blocks, data)}
 
